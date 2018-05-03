4b7c8bbffe6821979e552e29d0ebc2ed0fe0494c
Eli Joshua Maravillas <ejtmaravillas@gmail.com>
2018-05-03 12:45:49 +0800

    Mean Stack Part 3 working update 2

--- models/user.js
+++ models/user.js
@@ -1,8 +1,14 @@
+//Schemas for User Data
+//USER DATA DETAILS and Database Storage
+
+//for database communication
 const mongoose = require('mongoose');
+//bycrypt for password encryption
 const bcrypt = require('bcryptjs');
+//configuration file for database
 const config = require('../config/database');
 
-//User Schema
+//User Schema for Mongoose (necessary fields for the User)
 const UserSchema = mongoose.Schema({
     name:{
         type: String
@@ -21,17 +27,24 @@ const UserSchema = mongoose.Schema({
     }
 });
 
+
+//variable User created so it can be used outside this file (module.exports)
+//User model with Schema UserSchema
 const User = module.exports = mongoose.model('User',UserSchema);
 
+//use models outside this file --- use module.exports.functionName
+//UserID
 module.exports.getUserById = function(id, callback){
     User.findById(id, callback);
 }
-
+//UserName
 module.exports.getUserByUsername = function(username, callback){
     const query = {username: username}
     User.findByOne(query, callback);
 }
 
+//addUser function used at User Register at users.js from routes folder
+//newUser.save saves the newUser fields to the database
 module.exports.addUser = function(newUser, callback){
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,21 @@ const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
+
+//gets the model data needed for User from user.js from models folder
 const User = require('../models/user');
 
 
-//Register route
+//Register User with SCHEMAS
 router.post('/register', (req, res, next) => {
+    //set up newUser object with "User" Schemas from models folder
     let newUser = new User({
         name: req.body.name,
         email: req.body.email,
         username: req.body.username,
         password: req.body.password
     });
+    //add User with Schemas required fields from object newUser
     User.addUser(newUser, (err, user) => {
         if(err){
             res.json({success: false, msg: 'Failed to register user'});
